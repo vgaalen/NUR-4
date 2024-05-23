7394abaceb6e688b5efe8641e7760824d4fe7f05
Remon van Gaalen <vangaalen@strw.leidenuniv.nl>
2024-05-23 14:12:12 +0200

    configured run.sh

--- run.sh
+++ run.sh
@@ -11,3 +11,24 @@ if [ ! -e "data/galaxy_data.txt" ]; then
     echo "Download galaxy_data.txt"
     wget -O data/galaxy_data.txt https://home.strw.leidenuniv.nl/~daalen/Handin_files/galaxy_data.txt
 fi
+
+if [! -e "fig1a.png" ]; then
+    echo "Run ex1.py"
+    python3 ex1.py
+fi
+
+if [! -e "fig2a.png" ]; then
+    echo "Run ex2.py"
+    python3 ex2.py
+fi
+
+if [! -e "fig3a.png" ]; then
+    echo "Run ex3.py"
+    python3 ex3.py
+fi
+
+echo "Generating the pdf"
+pdflatex NUR-4.tex > latex_output1.txt
+#bibtex template.aux > bibtex_output.txt
+pdflatex NUR-4.tex > latex_output2.txt
+pdflatex NUR-4.tex > latex_output3.txt
